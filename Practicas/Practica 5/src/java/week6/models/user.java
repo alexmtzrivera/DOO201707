2d6e65c99cee953ac3c6ea6649f9e42cc97d000c
alexmtzrivera <j.alex.martinezrivera@gmail.com>
2017-03-07 23:00:00 -0600

    Add files via upload

---
+++ Practicas/Practica 5/src/java/week6/models/user.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6.models;
+
+/**
+ *
+ * @author JoseAlejandro
+ */
+public class user {
+    private String usuario;
+    private String password;
+    String email;
+    String nombre;
+    String apellido;
+    String ocupacion;
+    
+
+    public user() {
+        this.usuario = "Alexmri";
+        this.password="123";
+        email= "olafsa21@gmail.com";
+        nombre="Alejanro";
+        apellido="Martinez Rivera";
+        ocupacion="Estudiante";
+        
+    }
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+    
+    public String getOcupacion(){
+        return ocupacion;
+    }
+}
