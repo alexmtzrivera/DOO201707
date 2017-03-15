56c32d4a79af80dd4228738fb7349341e8386f3b
alexmtzrivera <j.alex.martinezrivera@gmail.com>
2017-03-14 22:09:33 -0600

    Add files via upload

---
+++ Practicas/Practica 6/src/java/week7/models/Authentication.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7.models;
+
+/**
+ *
+ * @author JoseAlejandro
+ */
+public class Authentication {
+    public static boolean authenticate(String usuario, String password) {
+
+        
+        user user = new user();
+        
+        if(usuario.equals(user.getUsuario()) && password.equals(user.getPassword())) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
